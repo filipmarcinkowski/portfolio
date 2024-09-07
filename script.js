c1ca6aa7d8e176c411ceb3f7e5652e4df303a54c
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-09-07 13:51:44 +0200

    change sections order

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 
 const title = document.querySelector('.section-title');
 const textBox = document.querySelectorAll('.project__text-box');
+const projectImage = document.querySelector('.project__image');
+const myImage = document.querySelector('.image');
 
 const changeMargin = function () {
-  const titleWidth = title.getBoundingClientRect().width + 'px';
+  const titleWidth = title.getBoundingClientRect().width;
+  const imageWidth = projectImage.getBoundingClientRect().width;
+  const myImageWidth = myImage.getBoundingClientRect().width;
+
   textBox.forEach(function (box) {
-    box.style.marginLeft = titleWidth;
+    box.style.marginLeft = titleWidth + 'px';
   });
+
+  const myImageMarginRight = imageWidth - myImageWidth;
+  myImage.style.marginRight = myImageMarginRight + 'px';
 };
 
 changeMargin();
